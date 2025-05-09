9b1aa4bc03b5cb3886bf5dedf092d05f400e81a6
saturnvi <maxwang420@gmail.com>
2025-05-09 15:05:45 -0500

    market no longer a singleton

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
-	public static void main(String[] args) {
-	    DataReader.getStocks();
-	}
+
+    public static void main(String[] args) {
+        Market.initializeMarket();
+        DataReader.getStocks();
+    }
 }
--- Market.java
+++ Market.java
@@ -1,32 +1,25 @@
 import java.util.*;
 
 public class Market {
-    private static Market instance;
-	private ArrayList<Stock> stocks;
 
-	private Market() {
-	    // read in from checkpoint file
-		stocks = DataReader.getStocks();
-		System.out.println(stocks);
-	}
+    private static ArrayList<Stock> stocks;
 
-	public Stock getStockByTicker(String ticker) {
-	    for (Stock stock : getInstance().getStocks()) {
-			if (stock.getSymbol().equals(ticker)) {
-			    return stock;
-			}
-		}
-		return null;
-	}
+    public static void initializeMarket() {
+        // read in from checkpoint file
+        stocks = DataReader.getStocks();
+        System.out.println(stocks);
+    }
 
-	public ArrayList<Stock> getStocks() {
-	    return stocks;
-	}
+    public static Stock getStockByTicker(String ticker) {
+        for (Stock stock : getStocks()) {
+            if (stock.getSymbol().equals(ticker)) {
+                return stock;
+            }
+        }
+        return null;
+    }
 
-	public static Market getInstance() {
-	    if (instance == null) {
-	    instance = new Market();
-		}
-		return instance;
-	}
+    public static ArrayList<Stock> getStocks() {
+        return stocks;
+    }
 }
