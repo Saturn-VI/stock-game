8073ab54f6e4f5f98014181b5bde233f24681703
saturnvi <maxwang420@gmail.com>
2025-05-08 21:30:12 -0500

    convert Market to a singleton

--- Market.java
+++ Market.java
@@ -1,12 +1,32 @@
 import java.util.*;
 
 public class Market {
-
+    private static Market instance;
 	private ArrayList<Stock> stocks;
 
-	public Market() {
+	private Market() {
 	    // read in from checkpoint file
 		stocks = DataReader.getStocks();
 		System.out.println(stocks);
+	}
+
+	public Stock getStockByTicker(String ticker) {
+	    for (Stock stock : getInstance().getStocks()) {
+			if (stock.getSymbol().equals(ticker)) {
+			    return stock;
+			}
+		}
+		return null;
+	}
+
+	public ArrayList<Stock> getStocks() {
+	    return stocks;
+	}
+
+	public static Market getInstance() {
+	    if (instance == null) {
+	    instance = new Market();
+		}
+		return instance;
 	}
 }
