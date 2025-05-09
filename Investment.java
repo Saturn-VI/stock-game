edcf01a802e36252f34ef6c8aa7c423d4f8367af
saturnvi <maxwang420@gmail.com>
2025-05-09 15:05:19 -0500

    formatting?

--- Investment.java
+++ Investment.java
@@ -1,4 +1,5 @@
 public class Investment {
+
     private Stock stock;
     private int shares;
 
@@ -8,10 +9,11 @@ public class Investment {
     }
 
     /**
-    * Sells a certain amount of shares.
-    * @param sharesToSell the number of shares to sell
-    * @return how much money was gained, -1.0 if sharesToSell > shares in investment
-    */
+     * Sells a certain amount of shares.
+     * If sharesToSell > the amount of shares in the investment, all shares will be sold.
+     * @param sharesToSell the number of shares to sell
+     * @return how much money was gained
+     */
     public double sellShares(int sharesToSell) {
         if (sharesToSell > shares) return -1.0;
         shares -= sharesToSell;
@@ -19,22 +21,22 @@ public class Investment {
     }
 
     /**
-    * Buys a certain amount of shares.
-    * if sharesToBuy is more than can be bought with moneyAvailable, the maximum number possible will be bought.
-    * @param sharesToBuy the number of shares to buy
-    * @param moneyAvailable how much money can be spent on this purchase
-    * @return 2-element array: first element is how many stocks were bought, second element is how much it cost
-    */
+     * Buys a certain amount of shares.
+     * if sharesToBuy is more than can be bought with moneyAvailable, the maximum number possible will be bought.
+     * @param sharesToBuy the number of shares to buy
+     * @param moneyAvailable how much money can be spent on this purchase
+     * @return 2-element array: first element is how many stocks were bought, second element is how much it cost
+     */
     public double[] buyShares(int sharesToBuy, double moneyAvailable) {
         if (sharesToBuy <= 0) {
-            return new double[]{0.0, 0.0};
+            return new double[] { 0.0, 0.0 };
         }
         int sharesThatCanBeBought = (int) (moneyAvailable / stock.getPrice());
         if (sharesThatCanBeBought == 0) {
-            return new double[]{0.0, 0.0};
+            return new double[] { 0.0, 0.0 };
         }
         int sharesBought = Math.min(sharesToBuy, sharesThatCanBeBought);
-        return new double[]{sharesBought, stock.getPrice() * sharesBought};
+        return new double[] { sharesBought, stock.getPrice() * sharesBought };
     }
 
     public double getTotalValue() {
