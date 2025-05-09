214a8f261962e2cd942fc91db04a8d02d61fea47
saturnvi <maxwang420@gmail.com>
2025-05-08 21:18:50 -0500

    add tostring to Stock, add getters

--- Stock.java
+++ Stock.java
@@ -3,12 +3,32 @@ public class Stock {
 	private String symbol;
 	private String name;
 	private double price;
-	private long totalShares; // total shares that exist for this stock
+	private final long totalShares; // total shares that exist for this stock - this is not changed, just used to give more weight to certain stock movements
 
 	public Stock(String symbol, String name, double price, long totalShares) {
 	    this.symbol = symbol;
 		this.name = name;
 		this.price = price;
 		this.totalShares = totalShares;
+	}
+
+	public String getSymbol() {
+	    return symbol;
+	}
+
+	public String getName() {
+	    return name;
+	}
+
+	public double getPrice() {
+	    return price;
+	}
+
+	public long getTotalShares() {
+	    return totalShares;
+	}
+
+	public String toString() {
+	    return String.format("%s $%.2f", symbol, price);
 	}
 }
