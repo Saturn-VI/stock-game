9f69b3258467b58ba28da8cdccf0256e134dbbb4
Shaoyu Wang <shaoyuwang119@gmail.com>
2025-05-10 16:13:33 +0000

    added the Transaction immutable class
    The Investment.java class now returns an immutable object of type Transaction during a purchase/sell. Also it raises an exception if number of shares you want to buy is greater than how many you are able to buy, or if the number of shares you want to sell is greater than how many you own.

--- AbstractTrader.java
+++ AbstractTrader.java
@@ -3,22 +3,20 @@ import java.util.ArrayList;
 public abstract class AbstractTrader {
 
     private double money;
-    private ArrayList<Investment> portfolio;
+    private ArrayList<Investment> portfolio = new ArrayList<>();
+    private ArrayList<Transaction> transactions = new ArrayList<>();
 
     public AbstractTrader() {
         money = 0.0;
-        portfolio = new ArrayList<Investment>();
     }
 
     public AbstractTrader(double money) {
         this.money = money;
-        portfolio = new ArrayList<Investment>();
     }
 
-    public AbstractTrader(double money, ArrayList<Stock> stocks) {
+    public AbstractTrader(double money, ArrayList<Stock> stocksList) {
         this.money = money;
-        portfolio = new ArrayList<Investment>();
-        for (Stock stock : stocks) {
+        for (Stock stock : stocksList) {
             portfolio.add(new Investment(stock, 0));
         }
     }
@@ -37,24 +35,38 @@ public abstract class AbstractTrader {
 
     /**
      * Buys a certain amount of shares.
-     * Will spend all available money.
+     * If sharesToBuy > amount of shares that can be bought, it will throw an exception.
+     * Adds the transaction into transactions (the transaction method)
      * @param stock the stock for which to buy shares
      * @param sharesToBuy the amount of shares to buy
      */
     public void buyShares(Stock stock, int sharesToBuy) {
         Investment investment = getInvestmentForStock(stock);
-        double[] purchaseReturn = investment.buyShares(sharesToBuy, money);
-        money -= purchaseReturn[1];
+        try {
+            Transaction tr = investment.buyShares(sharesToBuy, money);
+            money += tr.price(); 
+            // it's  a += because the value of tr.price() will be negative when buying.
+            transactions.add(tr);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 
     /**
      * Sells a certain amount of shares.
-     * If sharesToSell > the amount that is invested, it will sell all shares.
+     * If sharesToSell > the amount that is invested, it will throw an exception.
+     * Adds the transaction into transactions (the transaction method)
      * @param stock the stock for which to sell shares
      * @param sharesToSell how many shares should be sold
      */
     public void sellShares(Stock stock, int sharesToSell) {
         Investment investment = getInvestmentForStock(stock);
-        money += investment.sellShares(sharesToSell);
+        try {
+            Transaction tr = investment.sellShares(sharesToSell);
+            money += tr.price();
+            transactions.add(tr);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 }
