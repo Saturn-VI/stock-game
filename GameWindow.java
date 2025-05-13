9df1920cf56127c1cb0131062fe11bf9a3ccf14d
saturnvi <maxwang420@gmail.com>
2025-05-13 15:55:35 -0500

    start work on table

--- GameWindow.java
+++ GameWindow.java
@@ -1,11 +1,9 @@
 import java.awt.*;
+import java.beans.Customizer;
 import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.table.AbstractTableModel;
 
 public class GameWindow {
-
-    // private final int gridWidth = 12;
-    // private final int gridHeight = 6;
 
     private JFrame gameFrame;
     private JPanel stockDisplayPanel, portfolioPanel;
@@ -29,8 +27,58 @@ public class GameWindow {
 
         gameFrame.add(tabbedPane);
 
-        gameFrame.setSize(400, 400);
+        gameFrame.setSize(600, 600);
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setVisible(true);
+    }
+
+    class PortfolioPanel extends JPanel {
+
+        public PortfolioPanel() {
+            super();
+        }
+
+        class CustomTableModel extends AbstractTableModel {
+
+            private static final String[] columnNames = new String[] {
+                "Name",
+                "Price",
+                "Shares Owned",
+                "Holding Value",
+            };
+
+            public String getColumnName(int col) {
+                return columnNames[col];
+            }
+
+            public int getRowCount() {
+                // TODO: Get the number of stocks that a trader has shares in
+                return 0;
+            }
+
+            public int getColumnCount() {
+                return columnNames.length;
+            }
+
+            public Object getValueAt(int row, int col) {
+                // TODO
+                return null;
+            }
+
+            public boolean isCellEditable(int row, int col) {
+                return false;
+            }
+
+            public void setValueAt(Object value, int row, int col) {
+                // does nothing
+            }
+        }
+    }
+
+    class StockDisplayPanel extends JPanel {
+
+        public StockDisplayPanel() {
+            super();
+        }
     }
 }
